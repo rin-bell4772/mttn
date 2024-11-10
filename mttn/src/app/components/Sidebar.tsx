9be450a426573fbb3f25a48c3d9ac90dcadbdc57
tmt83019 <tmt83019@uga.edu>
2024-11-10 01:38:55 -0500

    Implemented closing sidebar feature & added corresponding CSS

--- mttn/src/app/components/Sidebar.tsx
+++ mttn/src/app/components/Sidebar.tsx
@@ -1,12 +1,28 @@
+"use client";
+import { useEffect } from 'react';
 import Link from 'next/link';
 import styles from './Sidebar.module.css';
 
 export default function Sidebar() {
+    function handleClick() {
+        const sidebar = document.getElementById("sidebar");
+        if (sidebar) {
+            sidebar.style.width = "0";
+        }
+    }
+
+    useEffect(() => {
+        const sidebar = document.getElementById("sidebar");
+        if (sidebar) {
+            sidebar.style.transition = "width 0.5s"; 
+        }
+    }, []);
+
     return (
-        <div className={styles.sidebar}>
+        <div id="sidebar" className={styles.sidebar}>
             <div>
                 <h1>NAVIGATION</h1>
-                <Link href="">X</Link> { /* Change later */ }
+                <Link href="#" onClick={handleClick} className={styles.closeButton}>&times;</Link>
             </div>
             <Link href="./dashboard">Dashboard</Link>
             <Link href="./settings">Account Settings</Link>
