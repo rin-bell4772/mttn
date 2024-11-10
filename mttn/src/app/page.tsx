5b473aee91d14fe4e22ee7256faab250d0ad1bf6
rin.bell <rin.bell4772@gmail.com>
2024-11-09 20:38:25 -0500

    added a comment

--- mttn/src/app/page.tsx
+++ mttn/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Nav from "./components/Nav";
 
-
+// This is going to be the Landing page
 export default function Home() {
   return (
     <>
