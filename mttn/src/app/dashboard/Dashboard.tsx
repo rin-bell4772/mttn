bb6a6e79ef23082d2eae9e4510c1c108cd9a8f91
tmt83019 <tmt83019@uga.edu>
2024-11-10 20:35:18 -0500

    Added nav & footer to dashboardPage, worked on dashboard sections but did not add to page yet, created 'AddStudySet' component files

---
+++ mttn/src/app/dashboard/Dashboard.tsx
@@ -0,0 +1,21 @@
+import styles from './Dashboard.module.css';
+import StudySet from '../components/StudySet';
+
+export default function Dashboard() {
+    return (
+        <div className={styles.dashboard}>
+            <h1>RECENT</h1>
+            <div>
+                
+            </div>
+            <h1>FAVORITES</h1>
+            <div>
+                
+            </div>
+            <h1>ALL</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
