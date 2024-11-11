a9fd1e0a974f5f48d0ff287831251c598db7d8c3
tmt83019 <tmt83019@uga.edu>
2024-11-11 14:21:10 -0500

    Add study set button now goes to add flashcard set page

--- mttn/src/app/components/AddStudySet.tsx
+++ mttn/src/app/components/AddStudySet.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link  from 'next/link';
 import Image from 'next/image';
 import styles from './AddStudySet.module.css';
 import add_icon from '../images/add_icon.png';
@@ -6,7 +7,9 @@ import add_icon from '../images/add_icon.png';
 export default function AddStudySet() {
     return (
         <div className={styles.addStudySet}>
-            <Image src={add_icon} alt="Add Icon"/>
+            <Link href="../flashcardSet">
+                <Image src={add_icon} alt="Add Icon"/>
+            </Link>
         </div>
     );
 }
