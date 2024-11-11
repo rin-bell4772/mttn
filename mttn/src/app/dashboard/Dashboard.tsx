ccdb55dcf593cedf2dfc4a861edc27d05e59e2cf
tmt83019 <tmt83019@uga.edu>
2024-11-10 23:44:28 -0500

    Created dummy array, doesn't do dynamic list stuff yet (to be done later)

--- mttn/src/app/dashboard/Dashboard.tsx
+++ mttn/src/app/dashboard/Dashboard.tsx
@@ -2,20 +2,39 @@ import styles from './Dashboard.module.css';
 import AddStudySet from '../components/AddStudySet';
 import StudySet from '../components/StudySet';
 
+// Dummy array of recent sets
+const recent = [
+    { title: 'Linear Algebra' },
+    { title: 'Chemistry' },
+    { title: 'French' },
+    { title: 'Calculus II' },
+    { title: 'History' }
+];
+
+const favorites = [];
+
 export default function Dashboard() {
     return (
         <div className={styles.dashboard}>
            <h1>RECENT</h1>
-           <div>
-                <AddStudySet />
+           <div className={styles.scroll}>
+                {recent.map((set, index) => (
+                    <StudySet key={index} title={set.title} />
+                ))}
            </div>
            <h1>FAVORITES</h1>
             <div className={styles.scroll}>
                 <AddStudySet />
+                {recent.map((set, index) => (
+                    <StudySet key={index} title={set.title} />
+                ))}
             </div>
            <h1>ALL</h1>
            <div className={styles.grid}>
                 <AddStudySet />
+                {recent.map((set, index) => (
+                    <StudySet key={index} title={set.title} />
+                ))}
            </div>
         </div>
     );
