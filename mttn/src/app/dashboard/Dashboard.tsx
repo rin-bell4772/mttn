66c71e64586594ecbe6cd77287c555c6e6bf8074
tmt83019 <tmt83019@uga.edu>
2024-11-10 21:44:15 -0500

    Fixed CSS for StudySet & AddStudySet in scrollable flexbox ('Favorites' section)

--- mttn/src/app/dashboard/Dashboard.tsx
+++ mttn/src/app/dashboard/Dashboard.tsx
@@ -5,24 +5,18 @@ import StudySet from '../components/StudySet';
 export default function Dashboard() {
     return (
         <div className={styles.dashboard}>
-            <h1>RECENT</h1>
-            <div>
-                
-            </div>
-            <h1>FAVORITES</h1>
+           <h1>RECENT</h1>
+           <div>
+                <AddStudySet />
+           </div>
+           <h1>FAVORITES</h1>
             <div className={styles.scroll}>
-                <StudySet title={"Name"}/>
-                <StudySet title={"Name"}/>
-                <StudySet title={"Name"}/>
-                <StudySet title={"Name"}/>
-                <StudySet title={"Name"}/>
-                <StudySet title={"Name"}/>
-                <StudySet title={"Name"}/>
+                <AddStudySet />
             </div>
-            <h1>ALL</h1>
-            <div className={styles.grid}>
-                
-            </div>
+           <h1>ALL</h1>
+           <div className={styles.grid}>
+                <AddStudySet />
+           </div>
         </div>
     );
 }
