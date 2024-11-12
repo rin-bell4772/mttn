06c390be73b1bf8d6308ff9681012fcba19a2b0a
Aishu Madanu <aishwini@gmail.com>
2024-11-11 20:00:02 -0500

    flashcard sets
    most of it lol

--- mttn/src/app/flashcardSet/flashcardSet.tsx
+++ mttn/src/app/flashcardSet/flashcardSet.tsx
@@ -1,26 +1,47 @@
 import styles from './flashcardSet.module.css';
+import Image from 'next/image';
+import backArrow from '../images/backArrow.png';
+import leftArrow from '../images/leftArrow.png';
+import rightArrow from '../images/rightArrow.png';
+import flipArrow from '../images/flipArrow.png';
+import star from '../images/star.png';
+import speaker from '../images/speaker.png';
+import shuffleSet from '../images/shuffleSet.png';
+import Link from 'next/link';
 
 export default function FlashcardSet() {
     return (
       
         <div className={styles.flashcardPage}>
-            <h1>TITLE</h1>
-            <div className={styles.cardArrows}>
-                <p className={styles.arrows}>&#8592;</p>
+            <div className={styles.heading}>
+                <Link href="/dashboard">
+                    <Image src={backArrow} alt={"arrow"}/>
+                </Link>
+                <h1>TITLE</h1>
+            </div>
+
+           <div className={styles.mainCard}>
+                <Image src={leftArrow} alt={"left arrow"} width={50}/>
                 <div className={styles.card}>
                     <div className={styles.upper}>
                         <p>1 of 10</p>
-                        <p>&#11088;</p>
+                        <Image src={star} alt={"star"} width={20}/>
                     </div>
-                    <p className={styles.termDef}>term/definition</p>
-                    <p className={styles.flipArrow}>&#8631;</p>
+                    <p className={styles.term}> Card</p>  
+                    <Image src={flipArrow} alt={"flip"} width={20}/>
                 </div>
-                <p className={styles.arrows}>&#8594;</p>
-   
+                <Image src={rightArrow} alt={"right arrow"} width={50}/>
+                        
+           </div>
+
+           <div className={styles.shuffling}> 
+              <Image src={shuffleSet} alt={"shuffle"} width={150}/>
             </div>
-            <p>Shuffle Set &#10227;</p>
-            
-            <p className={styles.time}>Time left: 0:00</p>
+
+            <div className={styles.bottom}>
+                <Image src={speaker} alt={"sound"} width={40}/>
+                <p className={styles.time}>Time left: 00:00</p>
+            </div>
          </div>
          
         
