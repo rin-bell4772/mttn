9d2f4e91f19cd72fffa8ffc2bc03e6b24dfe34e8
tmt83019 <tmt83019@uga.edu>
2024-11-20 16:22:46 -0500

    Deleted recents

--- mttn/src/app/dashboard/Dashboard.tsx
+++ mttn/src/app/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import AddStudySet from '../components/AddStudySet';
 import StudySet from '../components/StudySet';
 
 // Dummy array of recent sets (think of as queue)
-const recent = [
+const all = [
     { title: 'Linear Algebra' },
     { title: 'Chemistry' },
     { title: 'French' },
@@ -13,28 +13,20 @@ const recent = [
 
 const favorites = [];
 
-const all = [];
-
 export default function Dashboard() {
     return (
         <div className={styles.dashboard}>
-           <h1>RECENT</h1>
-           <div className={styles.grid}>
-                {recent.slice(0, 5).map((set, index) => (
-                    <StudySet key={index} title={set.title} />
-                ))}
-           </div>
            <h1>FAVORITES</h1>
             <div className={styles.scroll}>
                 <AddStudySet />
-                {recent.map((set, index) => (
+                {all.map((set, index) => (
                     <StudySet key={index} title={set.title} />
                 ))}
             </div>
            <h1>ALL</h1>
            <div className={styles.grid}>
                 <AddStudySet />
-                {recent.map((set, index) => (
+                {all.map((set, index) => (
                     <StudySet key={index} title={set.title} />
                 ))}
            </div>
