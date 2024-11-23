ddf93cdbe8d08b9534072b963fb4bfc28af03d13
rin.bell <rin.bell4772@gmail.com>
2024-11-23 10:37:54 -0500

    some progress on authentication and authorization

---
+++ mttn/src/auth.ts
@@ -0,0 +1,54 @@
+import { authConfig } from "./auth.config";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/models/userSchema";
+
+const bcrypt = require('bcrypt');
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
+    ...authConfig,
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            async authorize(credentials) {
+                if (!credentials) return null;
+
+                try {
+                    const user = await User.findOne({ email: credentials.email }).lean();
+
+                    if (user) {
+                        const isMatch = await bcrypt.compare(
+                            credentials.password,
+                            user.password
+                        );
+
+                        if (isMatch) {
+                            return {
+                                if: user._id.toString(),
+                                email: user.email,
+                                name: user.username,
+                            };
+                        } else {
+                            console.log("Email or Password is not correct");
+                            return null;
+                        }
+                    } else {
+                        console.log("User not found");
+                        return null;
+                    }
+                } catch (error:any) {
+                    console.log("An error occured: ", error);
+                    return null;
+                } 
+            }
+        })
+    ]
+})
