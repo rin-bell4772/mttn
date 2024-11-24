a01f6f5777a97dae56d97cba19b6e52544034639
rin.bell <rin.bell4772@gmail.com>
2024-11-24 17:42:32 -0500

    moved sounds + commented out errrors

--- mttn/src/middleware.ts
+++ mttn/src/middleware.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
-import { auth } from "./auth";
+//import { auth } from "./auth";
+import { authConfig } from "./auth.config";
 import NextAuth from 'next-auth';
+
+const { auth } = NextAuth(authConfig); 
 
 export default async function middleware(request: any) {
     const { nextURL } = request;
