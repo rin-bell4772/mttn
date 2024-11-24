8e9a5835f669abae3b3285a0fa2d10abf78a7fec
rin.bell <rin.bell4772@gmail.com>
2024-11-24 11:40:16 -0500

    not really sure what changed here

--- mttn/src/app/api/users/route.ts
+++ mttn/src/app/api/users/route.ts
@@ -3,6 +3,7 @@ import User from "@/models/userSchema";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+const bcrypt = require('bcrypt');
 
 // tested
 // get all users
@@ -19,8 +20,19 @@ export async function GET(request: NextRequest) {
 // create a user
 export async function POST(request: NextRequest) {
     // Handle POST requests
-    const { username, email, password, profilePicture, sets } = await request.json();
+    const { username, email, password, profilePicture } = await request.json();
     await connectMongoDB();
-    await User.create({ username, email, password, profilePicture, sets });
-    return NextResponse.json({ message: "Item added successfully" }, { status: 201 });
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const newUser = {
+        username,
+        email,
+        password: hashedPassword,
+        profilePicture,
+    }
+    try {
+        await User.create(newUser);
+        return NextResponse.json({ message: "Item added successfully" }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error creating a user" }, { status: 500 });
+    }
 }
