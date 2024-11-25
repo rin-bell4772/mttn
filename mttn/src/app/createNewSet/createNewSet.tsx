d8cfadc2e6e172317a83f4650780c47162de02c7
tmt83019 <tmt83019@uga.edu>
2024-11-24 19:30:53 -0500

    Centered AddFlashcard in createNewSet

--- mttn/src/app/createNewSet/createNewSet.tsx
+++ mttn/src/app/createNewSet/createNewSet.tsx
@@ -1,7 +1,7 @@
 "use client"
 import styles from './CreateNewSet.module.css';
 import Card from '../components/Card';
-import Flashcard from './Flashcard';
+import Flashcard from './flashcard';
 import Button from '../components/Button';
 import AddFlashcard from './AddFlashcard';
 import Link from 'next/link';
