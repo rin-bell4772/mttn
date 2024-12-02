58d2b1df4ec7853305a07bc2c52ceda73839e296
rin.bell <rin.bell4772@gmail.com>
2024-12-02 17:41:40 -0500

    delete works i guess

--- mttn/src/app/dashboard/Dashboard.tsx
+++ mttn/src/app/dashboard/Dashboard.tsx
@@ -16,27 +16,28 @@ export default function Dashboard() {
     const { data: session } = useSession();
     const userId = session?.user?.id;
 
+    const fetchStudySets = async () => {
+        try {
+            if (!userId) {
+                console.error("User ID is not available");
+                return;
+            }       
+
+            const response = await fetch(`/api/users/${userId}/sets`);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+                
+            const data = await response.json();
+            setStudySets(data.sets);
+        } catch (error) {
+            console.error("Error fetching study sets:", error);
+        }
+    }
+
     // GET request for all sets
     useEffect(() => {
-        const fetchStudySets = async () => {
-            try {
-                if (!userId) {
-                    console.error("User ID is not available");
-                    return;
-                }       
-
-                const response = await fetch(`/api/users/${userId}/sets`);
-
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                    
-                const data = await response.json();
-                setStudySets(data.sets);
-            } catch (error) {
-                console.error("Error fetching study sets:", error);
-            }
-        }
         fetchStudySets();
     }, [session]);
 
@@ -46,7 +47,7 @@ export default function Dashboard() {
             <div className={styles.grid}>
                 <AddStudySet />
                 {studySets.map((set, index) => (
-                    <StudySet key={index} id={set._id} title={set.name} />
+                    <StudySet key={index} id={set._id} title={set.name} onDelete={fetchStudySets} />
                 ))}
             </div>
         </div>
