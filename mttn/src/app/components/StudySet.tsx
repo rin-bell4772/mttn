aa99c9253746cc6c7a2c2b0300c5e3f8db3894b3
tmt83019 <thu7560@gmail.com>
2024-12-02 16:29:59 -0500

    Updated StudySet.tsx & CSS

--- mttn/src/app/components/StudySet.tsx
+++ mttn/src/app/components/StudySet.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import styles from './StudySet.module.css';
+import { useSession } from "next-auth/react";
 
 interface StudySetProps {
     title: string;
 }
 
 export default function StudySet(props: StudySetProps) {
+    const { data: session } = useSession();
+
+    const handleEdit = () => {
+        // Insert edit code here
+    };
+
+    const handleDelete = () => {
+        // Insert delete code here
+    };
+
+    const handleClick = () => {
+        // Insert code here
+    };
+
     return (
-        <div className={styles.studySet}>
-            <p>{props.title}</p>
-            <button className={styles.editButton}>Edit</button>
-            <button className={styles.deleteButton}>Delete</button>
+        <div className={styles.studySet} onClick={handleClick}>
+                <p>{props.title}</p>
+                <button className={styles.editButton}>Edit</button>
+                <button className={styles.deleteButton} onClick={handleDelete}>Delete</button>
         </div>
     );
 }
